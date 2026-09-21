fd68474364a984d7073943d06b9feae5e9419afd
christophe76430 <christophe.dellacasa@gmail.com>
2026-09-21 18:04:47 +0200

    Add files via upload

--- netlify/functions/admin-documents.mjs
+++ netlify/functions/admin-documents.mjs
@@ -177,7 +177,7 @@ function documentsPage(docs, message = '') {
     </div>
 
     <p style="margin-top:25px">
-      <a href="/admin">← Retour à l'administration</a>
+      <a href="/.netlify/functions/admin">← Retour à l'administration</a>
     </p>
 
     <script>
